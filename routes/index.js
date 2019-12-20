df18444f84dfdc895ee67175a722edf45c641e5f
ken20001207 <varedx@gmail.com>
2019-12-20 18:09:48 +0800

    ver1.0

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+fs = require('fs');
 var router = express.Router();
 const formidable = require('formidable')
 
@@ -25,9 +26,24 @@ router.post('/upload', function (req, res) {
     console.log("\n开始使用指定算法 " + algorithm + " 进行计算 ...\n");
     /* 在这里使用指定的算法 algorithm 处理接收到的文件 file */
 
-    console.log("\n计算完成！发送回应至前端。\n");
-    res.sendFile(file.path);
+    if (file.type.split('/')[0] == "image") {
+      console.log("\n计算完成！储存至 " + 'public/temp.' + file.type.split('/')[1] + "\n");
 
+      fs.readFile(file.path, function (_err, data) {
+        fs.writeFile("public/temp." + file.type.split('/')[1], data, function () {
+        });
+      });
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify({ "filename": "temp." + file.type.split('/')[1] }));
+    } else if (file.type.split('/')[0] == "text") {
+      res.setHeader('Content-Type', 'application/json');
+      fs.readFile(file.path, 'utf-8',(err, data) => { 
+        if (err) throw err; 
+      
+        
+      res.end(JSON.stringify({ "text": data }));
+    }) 
+    }
   })
 })
 
